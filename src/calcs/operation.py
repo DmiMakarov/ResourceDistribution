00081c997d13b5a4c6fa33edd908a0e960582ec6
Dmitry Makarov <makarov.da@phystech.edu>
2024-10-27 11:58:29 +0300

    Move to source

--- src/calcs/operation.py
+++ src/calcs/operation.py
@@ -1,8 +1,8 @@
 '''Define operation class'''
-from calcs.resource import Resource
-from calcs.employee import Employee
+from src.calcs.resource import Resource
+from src.calcs.employee import Employee
 
-class LinearOperation():
+class Operation():
     '''
     Define linear oparation class
 
@@ -25,7 +25,7 @@ class LinearOperation():
         '''Calculate standart time'''
         return count * self.standart_time
     
-class PiecewiseOperation(LinearOperation):
+class ConveyorOperation(Operation):
     '''
     Define piecewise-linear oparation class
 
