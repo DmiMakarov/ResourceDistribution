d6b4f81a0a7195e3d06db0feb3529b09acd5dbf5
Dmitry Makarov <makarov.da@phystech.edu>
2024-12-11 00:31:15 +0300

    a

--- src/calcs/shifts.py
+++ src/calcs/shifts.py
@@ -6,7 +6,10 @@ Main idea is to accumulate info about previous operation during one shift
 
 import datetime
 
-class ShiftOperation():
+import pandas as pd
+
+
+class ShiftOperation:
 
     def __init__(self,
                  operation_name: str,
@@ -14,6 +17,7 @@ class ShiftOperation():
                  prev_operations: dict[str, int],
                  next_operations: set[str],
                  ) -> None:
+        self.count: int = 0
         self.operation_name: str = operation_name
         self.detail_per_hour: dict[str, float] = detail_per_hour
         #for parallel operations
@@ -73,19 +77,84 @@ class ShiftOperation():
 
 #что мне теперь надо для вычислений
 #1. Составить конфигурации всех деталей
-#2. Заполнить detail_per_hour (идеально по конфигу, но пофиг, пока так сделаем)
+#2. Заполнить detail_per_hour (идеально по конфигу, но пофиг, пока так сделаем) - done
 #3. Написать цикл вычислений
 #4. Собрать в одну таблицу
 
 class ShiftCalc:
-
     def __init__(self,
-                 shifts: list[ShiftOperation]):
-        self.shifts: list[ShiftOperation] = shifts
+                 shifts: dict[str, list[ShiftOperation]]) -> None:
+        self.shifts: dict[str, list[ShiftOperation]] = shifts
 
-    def calc():
+    #строго говоря, тут всё надо распихать по струкутрам - operations, configs
+    def calc(self,
+             operations: dict[str, pd.DataFrame],
+             input_count: dict[str, int]) -> pd.DataFrame:
+
+        self.__fill_operations(operations=operations, input_count=input_count)
+
+        details_to_compute: list[str] = list(operations.keys())
+
         while true:
             current_date+=1
 
             if total_doors == required_doors:
                 break
+
+    def __fill_operations(self,
+                          operations: dict[str, pd.DataFrame],
+                          input_count: dict[str, int]) -> None:
+
+        map_operations: dict[str, dict] = {
+            "Оператор станок с пу/лазер|Лазерная резка листа":
+             {
+             "machine": 1,
+             "people": 1
+            },
+            "Станочник широкого профиля|Вертикально-фрезерная":
+             {"machine": 1,
+             "people": 1
+            },
+            "Станочник широкого профиля|Токарная":
+             {"machine": 1,
+             "people": 1
+            },
+            "Слесарь по сборке|Сборочная":
+             {"machine": 5,
+             "people": 5
+            },
+            "Слесарь по сборке|Упаковочная":
+             {"machine": 2,
+             "people": 2
+            },
+            "Слесарь по сборке|Ленточно-отрезная":
+             {"machine": 2,
+             "people": 2
+            },
+            "Слесарь по сборке|Слесарная":
+             {"machine": 1,
+             "people": 2
+            },
+            "Эл. Сварщик и п/авт машин|Сварка полуавтоматом в среде защитного газа (MIG)":
+             {"machine": 1,
+             "people": 1
+            },
+            "Оператор станок с пу/гибка|Листосгибочная":
+             {"machine": 1,
+             "people": 1
+            },
+            "Оператор станок с пу/гибка|Вальцовочная":
+             {"machine": 1,
+             "people": 1
+            },
+            "Оператор окрасочно-сушильной линии и агрегата|Окрашивание порошком":
+             {"machine": 1,
+             "people": 6 #TODO (me): Уточнить
+            }
+        }
+
+        for detail in self.shifts:
+            for shift_operation in self.shifts[detail]:
+                shift_operation.count = map_operations[shift_operation.operation_name]["people"]
+                shift_operation.detail_per_hour = input_count['detail'] /  \
+                                                  operations[operations["Operation"] == shift_operation.operation_name.split("|")[1]]["Time"].to_numpy()[0]
